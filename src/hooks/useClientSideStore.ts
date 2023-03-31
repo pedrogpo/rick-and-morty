7a34fc7d4d7133be65aba08c76ce74b5f4e15a86
Pedro Guilherme <pedroguilhermepedromen47@gmail.com>
2023-03-31 17:10:40 -0300

    docs: add docs for useClientSideStore

--- src/hooks/useClientSideStore.ts
+++ src/hooks/useClientSideStore.ts
@@ -1,5 +1,25 @@
 import { useEffect, useState } from 'react'
 
+/**
+ * useClientSideStore - A custom React hook to handle client-side store hydration.
+ *
+ * This hook is designed to address the hydration issue, delaying the store update
+ * until after the useEffect hook has run. This ensures the store is treated correctly
+ * during the hydration process.
+ *
+ * @template T - The type of the store.
+ * @param {T} store - The initial store value.
+ * @returns {T | null} - The locally stored value of type T or null if the state has not been set yet.
+ *
+ * @example
+ * import useClientSideStore from './useClientSideStore';
+ *
+ * function App() {
+ *   const localStore = useClientSideStore(store);
+ *
+ *   // Use localStore in your component
+ * }
+ */
 export default function useClientSideStore<T>(store: T): T | null {
   const [localStore, setLocalStore] = useState<T | null>(null)
 
