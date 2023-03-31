563ae3b0cde94a7409cf0eb53df1108caa30343a
Pedro Guilherme <pedroguilhermepedromen47@gmail.com>
2023-03-31 13:27:56 -0300

    feat(Axios): add axios-cache-adapter

--- src/core/http/api.ts
+++ src/core/http/api.ts
@@ -1,8 +1,12 @@
+import 'axios-cache-adapter'
 import axios, { AxiosResponse } from 'axios'
 import { handleError } from './errors'
 
 export const rmApi = axios.create({
   baseURL: 'https://rickandmortyapi.com/api/',
+  cache: {
+    maxAge: 15 * 60 * 1000,
+  },
 })
 
 rmApi.interceptors.response.use(
